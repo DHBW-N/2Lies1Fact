3f65af9bfa32ace74ae8cbc18d8446db81054acd
bezwinger <bezwingerweb@tutanota.com>
2025-07-12 18:29:19 +0200

    Accessibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({ onBack }: { onBack: () => void }) {
                     pointerEvents: "auto",
                 }}
             >
-                ⬅️ Zurück
+                ⬅️ Back to main page
             </button>
             <h1 style={{ margin: "0 0 10px 0", pointerEvents: "auto" }}>2 LIES 1 FACT</h1>
         </div>
