009ba004829eac77baf9ab5b5feec38f14b33891
eincoder3000 <fs-et@web.de>
2025-07-25 15:59:10 +0200

    chore refactor testfiles

--- src/test/__mocks__/fileMock.js
+++ src/test/__mocks__/fileMock.js
@@ -1,1 +1,2 @@
+// This is a mock file for Jest to handle static assets like images
 module.exports = 'test-file-stub';
--- src/test/types/images.d.ts
+++ src/test/types/images.d.ts
@@ -1,3 +1,4 @@
+// This file is used to declare modules for image files in TypeScript.
 declare module "*.png" {
     const value: string;
     export default value;
